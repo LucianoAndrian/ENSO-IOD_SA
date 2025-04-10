f5a69939d81fecf8332730939efede4d144e6618
LucianoAndrian <lucianoandrian@hotmail.com>
2025-04-10 18:05:09 -0300

    MC cfsv2 - error random

---
+++ 10_MonteCarloTest_for_CFSv2Composite.py
@@ -0,0 +1,167 @@
+"""
+Test de Monte Carlo para las composiciones de eventos observados
+"""
+# ---------------------------------------------------------------------------- #
+save = False
+out_dir = ('/pikachu/datos/luciano.andrian/observado/ncfiles/'
+           'CFSv2_nc_quantiles/')
+
+variables = ['prec', 'tref']
+
+cases = ['sim_pos', 'sim_neg',
+         'dmi_puros_pos', 'dmi_puros_neg',
+         'n34_puros_pos', 'n34_puros_neg']
+
+# ---------------------------------------------------------------------------- #
+import xarray as xr
+import numpy as np
+from multiprocessing.pool import ThreadPool
+import os
+import glob
+import math
+from datetime import datetime
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+
+# ---------------------------------------------------------------------------- #
+dir_events = '/pikachu/datos/luciano.andrian/cases_fields/'
+
+# Functions ------------------------------------------------------------------ #
+def CompositesSimple_CFSv2(data, index):
+    try:
+        data_sel = data.sel(position=index, drop=True)
+    except:
+        print('Error select')
+        print('Trying wo drop')
+        data_sel = data.sel(position=index)
+    return data_sel.mean('position')
+
+def NumberPerts(data_to_concat, neutro, num = 0):
+    #si las permutaciones posibles:
+    # > 10000 --> permutaciones = 10000
+    # < 1000 ---> permutaciones = todas las posibles
+    #
+    # si num != 0 permutaciones = num
+
+    total = len(data_to_concat.position) + len(neutro.position)
+    len1 = len(neutro.position)
+    len2 = len(data_to_concat.position)
+
+    try:
+        total_perts = math.factorial(total) / (math.factorial(len2) * \
+                                           math.factorial(len1))
+    except OverflowError:
+        total_perts = 10000
+
+    if num == 0:
+        if total_perts >= 10000:
+            tot = 10000
+            print('M = 10000')
+        else:
+            tot = total_perts
+            print('M = ' + str(total_perts))
+
+    else:
+        tot = num
+
+    jump = 9 #10 por .nc que guarde
+    M = []
+    n = 0
+
+    while n < tot:
+        aux = list(np.linspace((0 + n), (n + jump), (jump + 1)))
+        M.append(aux)
+        n = n + jump + 1
+
+    return M
+
+# ---------------------------------------------------------------------------- #
+for v in variables:
+    print(v)
+
+    neutro = xr.open_dataset(f'{dir_events}{v}_neutros_SON_detrend_05.nc')
+    len_neutro = len(neutro.time)
+    neutro['time'] = np.arange(len(neutro.time))
+    neutro = neutro.rename({'time': 'position'})
+
+    for c in cases:
+        print(c)
+        # Borrar archivos temporales ----------------------------------------- #
+        files = glob.glob('/pikachu/datos/luciano.andrian/'
+                          'observado/ncfiles/nc_comps/' + '*.nc')
+
+        if len(files) != 0:
+            for f in files:
+                try:
+                    os.remove(f)
+                except:
+                    print('Error: ' + f)
+        # -------------------------------------------------------------------- #
+
+        event = xr.open_dataset(f'{dir_events}{v}_{c}_SON_no_detrend.nc')
+        event = event.rename({'time': 'position'})
+
+        concat = xr.concat([neutro, event], dim='position')
+        concat['position'] = np.arange(len(concat.position))
+
+        def PermuDatesComposite(n, data=concat, len_neutro=len_neutro):
+            rn = np.random.RandomState(616)
+
+            for a in n:
+                dates_rn = rn.permutation(data.position)
+                neutro_new = dates_rn[0:len_neutro]
+                data_new = dates_rn[len_neutro:]
+
+                neutro_comp = CompositesSimple_CFSv2(data, neutro_new)
+                event_comp = CompositesSimple_CFSv2(data, data_new)
+
+                comp = event_comp - neutro_comp
+
+
+                if a == n[0]:
+                    comp_concat = comp
+                else:
+                    comp_concat = xr.concat([comp_concat, comp], dim='position')
+
+            comp_concat.to_netcdf(
+                '/pikachu/datos/luciano.andrian/observado'
+                '/ncfiles/nc_comps/' + 'Comps_' +
+                str(int(a)) + '.nc')
+
+            del neutro_comp, event_comp, comp
+
+        M = NumberPerts(concat, neutro, 0)
+
+        hour = datetime.now().hour
+        if (hour > 19) | (hour < 8):
+            n_thread = 20
+            pool = ThreadPool(20)
+        else:
+            n_thread = 10
+            pool = ThreadPool(10)
+
+        print(f'Threads: {n_thread}')
+
+        pool.map(PermuDatesComposite, [n for n in M])
+        pool.close()
+        print('poool ok')
+        # NO chunks! acumula RAM en cada ciclo. ~14gb en 3 ciclos...
+        aux = xr.open_mfdataset('/pikachu/datos/luciano.andrian/'
+                                'observado/ncfiles/'
+                                'nc_comps/Comps_*.nc', parallel=True,
+                                combine='nested', concat_dim="time",
+                                coords="different",
+                                compat="broadcast_equals")
+
+        print(f'Quantiles ------------------------------------------- ')
+        aux = aux.chunk({'time': -1})
+        qt = aux.quantile([.05, .95], dim='time',
+                          interpolation='linear')
+        qt.to_netcdf(f'{out_dir}{v}_QT_{c}_CFSv2_detrend_05.nc',
+                     compute=True)
+        aux.close()
+        del qt
+
+print('# ------------------------------------------------------------------- #')
+print('# ------------------------------------------------------------------- #')
+print('done')
+print('# ------------------------------------------------------------------- #')
