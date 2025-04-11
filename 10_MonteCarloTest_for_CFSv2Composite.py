c20a526ac2d4ea94ee2b0a201e388ebbed2411fb
LucianoAndrian <lucianoandrian@hotmail.com>
2025-04-11 17:14:54 -0300

    fix error, mp

--- 10_MonteCarloTest_for_CFSv2Composite.py
+++ 10_MonteCarloTest_for_CFSv2Composite.py
@@ -21,19 +21,22 @@ import glob
 import math
 from datetime import datetime
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+import gc
 
 # ---------------------------------------------------------------------------- #
 dir_events = '/pikachu/datos/luciano.andrian/cases_fields/'
 
 # Functions ------------------------------------------------------------------ #
 def CompositesSimple_CFSv2(data, index):
-    try:
-        data_sel = data.sel(position=index, drop=True)
-    except:
-        print('Error select')
-        print('Trying wo drop')
-        data_sel = data.sel(position=index)
-    return data_sel.mean('position')
+
+    index_array = np.array(index)
+    if len(index_array) != len(np.unique(index_array)):
+        raise ValueError("Valores duplicados en index")
+
+    data_sel = data.sel(position=index_array, drop=True)
+    data_sel = data_sel.mean('position')
+
+    return data_sel
 
 def NumberPerts(data_to_concat, neutro, num = 0):
     #si las permutaciones posibles:
@@ -80,8 +83,8 @@ for v in variables:
 
     neutro = xr.open_dataset(f'{dir_events}{v}_neutros_SON_detrend_05.nc')
     len_neutro = len(neutro.time)
-    neutro['time'] = np.arange(len(neutro.time))
     neutro = neutro.rename({'time': 'position'})
+    neutro = neutro.drop_vars(['r', 'L'])
 
     for c in cases:
         print(c)
@@ -95,18 +98,20 @@ for v in variables:
                     os.remove(f)
                 except:
                     print('Error: ' + f)
-        # -------------------------------------------------------------------- #
 
+        # -------------------------------------------------------------------- #
         event = xr.open_dataset(f'{dir_events}{v}_{c}_SON_no_detrend.nc')
         event = event.rename({'time': 'position'})
+        event = event.drop_vars(['r', 'L'])
 
         concat = xr.concat([neutro, event], dim='position')
+
         concat['position'] = np.arange(len(concat.position))
 
         def PermuDatesComposite(n, data=concat, len_neutro=len_neutro):
-            rn = np.random.RandomState(616)
 
             for a in n:
+                rn = np.random.RandomState(616 + int(a))
                 dates_rn = rn.permutation(data.position)
                 neutro_new = dates_rn[0:len_neutro]
                 data_new = dates_rn[len_neutro:]
@@ -116,50 +121,53 @@ for v in variables:
 
                 comp = event_comp - neutro_comp
 
-
                 if a == n[0]:
                     comp_concat = comp
                 else:
-                    comp_concat = xr.concat([comp_concat, comp], dim='position')
+                    comp_concat = xr.concat([comp_concat, comp],
+                                            dim='position')
 
-            comp_concat.to_netcdf(
-                '/pikachu/datos/luciano.andrian/observado'
-                '/ncfiles/nc_comps/' + 'Comps_' +
-                str(int(a)) + '.nc')
+            comp_concat.to_netcdf('/pikachu/datos/luciano.andrian/observado'
+                                  '/ncfiles/nc_comps/' + 'Comps_' +
+                                  str(int(a)) + '.nc')
 
-            del neutro_comp, event_comp, comp
+            del neutro_comp, event_comp, comp, comp_concat
+            gc.collect()
 
         M = NumberPerts(concat, neutro, 0)
-
+        import multiprocessing as mp
         hour = datetime.now().hour
+
         if (hour > 19) | (hour < 8):
-            n_thread = 20
-            pool = ThreadPool(20)
+            n_proc = 30
         else:
-            n_thread = 10
-            pool = ThreadPool(10)
+            n_proc = 20
 
-        print(f'Threads: {n_thread}')
+        print(f'Procesos: {n_proc}')
 
-        pool.map(PermuDatesComposite, [n for n in M])
-        pool.close()
-        print('poool ok')
+        with mp.Pool(processes=n_proc) as pool:
+            pool.map(PermuDatesComposite, [n for n in M])
+        print('pool ok')
+        del event, concat
+        gc.collect()
+
         # NO chunks! acumula RAM en cada ciclo. ~14gb en 3 ciclos...
         aux = xr.open_mfdataset('/pikachu/datos/luciano.andrian/'
                                 'observado/ncfiles/'
                                 'nc_comps/Comps_*.nc', parallel=True,
-                                combine='nested', concat_dim="time",
+                                combine='nested', concat_dim="position",
                                 coords="different",
                                 compat="broadcast_equals")
 
         print(f'Quantiles ------------------------------------------- ')
-        aux = aux.chunk({'time': -1})
-        qt = aux.quantile([.05, .95], dim='time',
+        aux = aux.chunk({'position': -1})
+        qt = aux.quantile([.05, .95], dim='position',
                           interpolation='linear')
         qt.to_netcdf(f'{out_dir}{v}_QT_{c}_CFSv2_detrend_05.nc',
                      compute=True)
         aux.close()
-        del qt
+        del qt, aux
+        gc.collect()
 
 print('# ------------------------------------------------------------------- #')
 print('# ------------------------------------------------------------------- #')
