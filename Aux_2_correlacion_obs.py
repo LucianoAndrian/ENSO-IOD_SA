4381523517d20021c924c5fa3ad839a32cc28d66
LucianoAndrian <lucianoandrian@hotmail.com>
2025-04-16 12:15:15 -0300

    add dmi and obs corr

--- Aux_2_correlacion_obs.py
+++ Aux_2_correlacion_obs.py
@@ -1,18 +1,17 @@
 """
-Correlacion entre N34 y prec y tref en CFSv2
+Correlacion entre N34, dmi y prec y tref
 """
 # ---------------------------------------------------------------------------- #
 save = True
 out_dir = '/home/luciano.andrian/doc/ENSO_IOD_SA/salidas/'
 
-dates_dir = '/pikachu/datos/luciano.andrian/cases_dates/'
-fields_dir = '/pikachu/datos/luciano.andrian/cases_fields/'
-index_dir = '/pikachu/datos/luciano.andrian/DMI_N34_Leads_r/'
+variables_tpp = ['ppgpcc_w_c_d_1', 'tcru_w_c_d_0.25']
 # ---------------------------------------------------------------------------- #
 import os
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 import xarray as xr
 from matplotlib import colors
+import numpy as np
 
 import warnings
 warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
@@ -20,8 +19,7 @@ from shapely.errors import ShapelyDeprecationWarning
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 warnings.filterwarnings("ignore")
 
-from Funciones import PlotFinal, SetDataToPlotFinal
-
+from Funciones import PlotFinal, SetDataToPlotFinal, Nino34CPC, DMI, SameDateAs
 # ---------------------------------------------------------------------------- #
 if save:
     dpi = 300
@@ -49,23 +47,56 @@ mintrose_divergent_13.set_under('#004D43')
 mintrose_divergent_13.set_over('#4C0035')
 mintrose_divergent_13.set_bad('white')
 
-# Correlation ---------------------------------------------------------------- #
+# Funciones ------------------------------------------------------------------ #
+def OpenObsDataSet(name, sa=True, dir='/pikachu/datos/luciano.andrian/'
+                                      'observado/ncfiles/data_obs_d_w_c/'):
 
-for i_name in ['N34', 'DMI']:
-    indice = xr.open_dataset(f'{index_dir}{i_name}_SON_Leads_r_CFSv2.nc')
-    indice = (indice - indice.mean())/indice.std()
+    aux = xr.open_dataset(dir + name + '.nc')
+
+    if sa:
+        aux2 = aux.sel(lon=slice(270, 330), lat=slice(15, -60))
+        if len(aux2.lat) > 0:
+            return aux2
+        else:
+            aux2 = aux.sel(lon=slice(270, 330), lat=slice(-60, 15))
+            return aux2
+    else:
+        return aux
+
+# Correlation ---------------------------------------------------------------- #
+print(' DMI y N34 ---------------------------------------------------------- #')
+dmi = DMI(filter_bwa=False, start_per=1920, end_per=2020)[2]
+dmi = dmi.sel(time=dmi.time.dt.month.isin(10)) # SON
+dmi = dmi.sel(time=dmi.time.dt.year.isin(np.arange(1940,2021)))
+dmi = (dmi - dmi.mean())/dmi.std()
+
+aux = xr.open_dataset(
+    "/pikachu/datos/luciano.andrian/verif_2019_2023/sst.mnmean.nc")
+n34 = Nino34CPC(aux, start=1920, end=2020)[0]
+n34 = n34.sel(time=n34.time.dt.month.isin(10)) # SON
+n34 = (n34 - n34.mean())/n34.std()
+n34 = SameDateAs(n34, dmi)
+
+
+for i, i_name in zip([n34, dmi], ['N34', 'DMI']):
 
     corr = []
-    for v in ['prec', 'tref']:
+    for v in variables_tpp:
+        data = OpenObsDataSet(name=v + '_SON', sa=False)
+        if v == variables_tpp[1]:
+            data['time'] = dmi.time
 
-        var = xr.open_dataset(f'{fields_dir}{v}_son_detrend.nc')
-        var = (var - var.mean(['r', 'time'])) / var.std(['r', 'time'])
+        data = SameDateAs(data, dmi)
+        var_name = list(data.data_vars)[0]
 
-        if v == 'tref':
-            var = var.sel(time=var.time.dt.year != 2011)
-            indice = indice.sel(time=indice.time.dt.year != 2011)
+        var = data.sel(lon=slice(275, 330), lat=slice(-60, 20))
 
-        corr.append(xr.corr(indice.sst, var[v], dim=['time', 'r']))
+        if len(var.lat) == 0:
+            var = data.sel(lon=slice(275, 330), lat=slice(20, -60))
+
+        var = (var - var.mean(['time'])) / var.std(['time'], skipna=True)
+
+        corr.append(xr.corr(i, var[var_name], dim=['time']))
 
     # Plot ------------------------------------------------------------------- #
     aux_v = SetDataToPlotFinal(corr[0], corr[1])
@@ -73,7 +104,7 @@ for i_name in ['N34', 'DMI']:
     PlotFinal(data=aux_v, levels=corr_scale,
               cmap=mintrose_divergent_13,
               titles=[f'corr. {i_name} vs Prec', f'corrs {i_name} vs Tref'],
-              namefig=f'corr_{i_name}_variables_CFSv2_pcolor',
+              namefig=f'corr_{i_name}_variables_OBS_pcolor',
               map='sa', save=save, dpi=dpi,
               out_dir=out_dir,
               data_ctn=aux_v, color_ctn='k',
