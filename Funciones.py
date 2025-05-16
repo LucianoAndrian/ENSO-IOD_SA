665f5211a65bd21345eebc386af6f2b4a5f43303
LucianoAndrian <lucianoandrian@hotmail.com>
2025-05-16 17:37:37 -0300

    funcion plot dos variables

--- Funciones.py
+++ Funciones.py
@@ -4011,6 +4011,201 @@ def PlotFinalFigS3(data, data_ctn, levels, cmap, title0, namefig, save, dpi,
     else:
         plt.show()
 
+def PlotFinalTwoVariables(data, num_cols,
+                          levels_r1, cmap_r1,
+                          levels_r2, cmap_r2,
+                          data_ctn, levels_ctn_r1, levels_ctn_r2, color_ctn,
+                          titles, namefig, save, dpi, out_dir, pdf=False,
+                          high=2, width = 7.08661, step=1,
+                          ocean_mask=False, num_cases=False,
+                          num_cases_data=None,
+                          sig_points=None, hatches=None,
+                          data_ctn_no_ocean_mask=False):
+
+    plots = data.plots.values
+    num_plots = len(plots)
+    num_rows = np.ceil(num_plots / num_cols).astype(int)
+
+    crs_latlon = ccrs.PlateCarree()
+
+    map == 'SA'
+    extent = [275, 330, -60, 20]
+    step_lon = 20
+    high = high
+
+    change_row = len(data.plots)/2
+
+    fig, axes = plt.subplots(
+        num_rows, num_cols, figsize=(width, high * num_rows),
+        subplot_kw={'projection': ccrs.PlateCarree(central_longitude=180)},
+        gridspec_kw={'wspace': 0.1, 'hspace': 0.2})
+
+    i = 0
+    for ax, plot in zip(axes.flatten(), plots):
+        no_plot = False
+
+        if plot < change_row:
+            levels = levels_r1
+            levels_ctn = levels_ctn_r1
+            cmap = cmap_r1
+            row1=True
+        else:
+            levels = levels_r2
+            levels_ctn = levels_ctn_r2
+            cmap = cmap_r2
+            row1 = False
+
+
+        # CONTOUR ------------------------------------------------------------ #
+        if data_ctn is not None:
+            if levels_ctn is None:
+                levels_ctn = levels.copy()
+            try:
+                if isinstance(levels_ctn, np.ndarray):
+                    levels_ctn = levels_ctn[levels_ctn != 0]
+                else:
+                    levels_ctn.remove(0)
+            except:
+                pass
+            aux_ctn = data_ctn.sel(plots=plot)
+            if aux_ctn.mean().values != 0:
+
+                if ocean_mask is True and data_ctn_no_ocean_mask is False:
+                    mask_ocean = MakeMask(aux_ctn)
+                    aux_ctn = aux_ctn * mask_ocean.mask
+
+                try:
+                    aux_ctn_var = aux_ctn['var'].values
+                except:
+                    aux_ctn_var = aux_ctn.values
+
+                ax.contour(data_ctn.lon.values[::step],
+                           data_ctn.lat.values[::step],
+                           aux_ctn_var[::step, ::step], linewidths=0.4,
+                           levels=levels_ctn, transform=crs_latlon,
+                           colors=color_ctn)
+
+        aux = data.sel(plots=plot)
+        if aux.mean().values != 0:
+
+            if ocean_mask is True:
+                mask_ocean = MakeMask(aux)
+                aux = aux * mask_ocean.mask
+
+            try:
+                aux_var = aux['var'].values
+            except:
+                aux_var = aux.values
+
+            if row1 is True:
+                im_r1= ax.contourf(aux.lon.values[::step],
+                                   aux.lat.values[::step],
+                                   aux_var[::step, ::step],
+                                   levels=levels,
+                                   transform=crs_latlon, cmap=cmap,
+                                   extend='both')
+            else:
+                im_r2 = ax.contourf(aux.lon.values[::step],
+                                    aux.lat.values[::step],
+                                    aux_var[::step, ::step],
+                                    levels=levels,
+                                    transform=crs_latlon, cmap=cmap,
+                                    extend='both')
+        else:
+            ax.axis('off')
+            no_plot=True
+
+
+        # sig ---------------------------------------------------------------- #
+        if sig_points is not None:
+            aux_sig_points = sig_points.sel(plots=plot)
+            if aux_sig_points.mean().values != 0:
+
+                if ocean_mask is True:
+                    mask_ocean = MakeMask(aux_sig_points)
+                    aux_sig_points = aux_sig_points * mask_ocean.mask
+
+                # hatches = '....'
+                colors_l = ['k', 'k']
+                try:
+                    comp_sig_var = aux_sig_points['var']
+                except:
+                    comp_sig_var = aux_sig_points.values
+                cs = ax.contourf(aux_sig_points.lon[::step],
+                                 aux_sig_points.lat[::step],
+                                 comp_sig_var[::step, ::step],
+                                 transform=crs_latlon, colors='none',
+                                 hatches=[hatches, hatches], extend='lower')
+
+                for i2, collection in enumerate(cs.collections):
+                    collection.set_edgecolor(colors_l[i2 % len(colors_l)])
+
+                for collection in cs.collections:
+                    collection.set_linewidth(0.)
+
+
+        # no plotear --------------------------------------------------------- #
+        if no_plot is False:
+            if num_cases:
+                ax.text(-0.005, 1.025, f"({string.ascii_lowercase[i]}), "
+                                       f"$N={num_cases_data[plot]}$",
+                        transform=ax.transAxes, size=6)
+            else:
+                ax.text(-0.005, 1.025, f"({string.ascii_lowercase[i]})",
+                        transform=ax.transAxes, size=6)
+            i = i + 1
+
+            ax.add_feature(cartopy.feature.LAND, facecolor='white',
+                           linewidth=0.5)
+            ax.coastlines(color='k', linestyle='-', alpha=1,
+                          linewidth=0.2,
+                          resolution='110m')
+
+            ax.add_feature(cartopy.feature.BORDERS, alpha=1,
+                               linestyle='-', linewidth=0.2, color='k')
+            gl = ax.gridlines(draw_labels=False, linewidth=0.1, linestyle='-',
+                              zorder=20)
+            gl.ylocator = plt.MultipleLocator(20)
+            ax.set_xticks(np.arange(0, 360, step_lon), crs=crs_latlon)
+            ax.set_yticks(np.arange(-80, 20, 20), crs=crs_latlon)
+            ax.tick_params(width=0.5, pad=1)
+            lon_formatter = LongitudeFormatter(zero_direction_label=True)
+            lat_formatter = LatitudeFormatter()
+            ax.xaxis.set_major_formatter(lon_formatter)
+            ax.yaxis.set_major_formatter(lat_formatter)
+            ax.tick_params(labelsize=4)
+            ax.set_extent(extent, crs=crs_latlon)
+
+            ax.set_aspect('equal')
+            ax.set_title(titles[plot], fontsize=6, pad=2)
+
+            for spine in ax.spines.values():
+                spine.set_linewidth(0.5)
+
+    pos1 = fig.add_axes([0.87, 0.55, 0.015, 0.35])
+    cb1 = fig.colorbar(im_r1, cax=pos1, orientation='vertical')
+    cb1.ax.tick_params(labelsize=5, pad=1)
+
+    pos2 = fig.add_axes([0.87, 0.1, 0.015, 0.35])
+    cb2 = fig.colorbar(im_r2, cax=pos2, orientation='vertical')
+    cb2.ax.tick_params(labelsize=5, pad=1)
+
+    fig.subplots_adjust(bottom=0.1, wspace=0.05, hspace=0.25, left=0.05,
+                        right=0.85, top=0.95)
+
+    if save:
+        if pdf is True:
+            plt.savefig(f"{out_dir}{namefig}.pdf", dpi=dpi, bbox_inches='tight')
+            plt.close()
+        else:
+            plt.savefig(f"{out_dir}{namefig}.png", dpi=dpi, bbox_inches='tight')
+            plt.close()
+    else:
+        plt.show()
+
+
+    plt.show()
+
 ################################################################################
 # PDF ##########################################################################
 def pdf_fit_normal(data, size, start, end):
